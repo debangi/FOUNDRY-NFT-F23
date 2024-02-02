5c01f4ee875f94562d489157b395d85997afaf23
Debangi <debangiofficial@gmail.com>
2024-02-02 15:19:29 +0530

    Created deploy script

--- script/DeployBasicNft.s.sol
+++ script/DeployBasicNft.s.sol
@@ -2,5 +2,13 @@
 pragma solidity ^0.8.18;
 
 import {Script} from "forge-std/Script.sol";
+import {BasicNft} from "../src/BasicNft.sol";
 
-contract DeployBasicNft {}
+contract DeployBasicNft is Script {
+    function run() external returns (BasicNft) {
+        vm.startBroadcast();
+        BasicNft basicNft = new BasicNft();
+        vm.stopBroadcast();
+        return basicNft;
+    }
+}
--- src/BasicNft.sol
+++ src/BasicNft.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract BasicNft is ERC721 {
     uint256 private s_tokenCounter;
